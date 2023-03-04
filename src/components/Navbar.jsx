66999890049548ef9e90de6eefe20daf77a2a078
Leonardo <oliveirabalsa2@gmail.com>
2023-03-04 03:01:59 -0300

    refactor: logo circle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,11 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={balsatech} alt="logo" className="w-9 h-9 object-contain" />
+          <img
+            src={balsatech}
+            alt="logo"
+            className="w-9 h-9 object-contain rounded-full"
+          />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Leonardo Balsalobre &nbsp;
             <span className="sm:block hidden"> | Software Engineer</span>
